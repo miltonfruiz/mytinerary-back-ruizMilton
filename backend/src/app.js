e27e6f96f157e7a6bc1994b96e25b3739b9f838d
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-10 17:42:56 -0300

    Configuración app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = require("./router/router");
 const app = express();
 
+app.use("/api", router);
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
