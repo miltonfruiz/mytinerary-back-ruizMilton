34696749cd4fc297e3b931ca46c6bc94002817af
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-15 14:25:13 -0300

    Creación middlewares

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -4,10 +4,13 @@ const {
   getClient,
   getClients,
   addClient,
+  deleteClient,
 } = require("../controller/clientsController");
+const { verifyDataClient } = require("../middlewares/verification");
 
 router.get("/clients", getClients);
 router.get("/client/:id", getClient);
-router.post("/clients", addClient);
+router.post("/clients", verifyDataClient, addClient);
+router.post("/clients", verifyDataClient, deleteClient);
 
 module.exports = router;
