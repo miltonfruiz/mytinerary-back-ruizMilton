464d57d56cb07c331f5c6996c5760a4bfc99766c
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-17 15:07:40 -0300

    Creación updateClient

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -5,6 +5,7 @@ const {
   getClients,
   addClient,
   deleteClient,
+  updateClient,
 } = require("../controller/clientsController");
 const { verifyDataClient } = require("../middlewares/verification");
 const { addAccount } = require("../controller/accountsController");
@@ -13,6 +14,7 @@ router.get("/clients", getClients);
 router.get("/clients/:id", getClient);
 router.post("/clients", verifyDataClient, addClient);
 router.delete("/clients", verifyDataClient, deleteClient);
+router.put("/clients", verifyDataClient, updateClient);
 router.post("/accounts", addAccount);
 
 module.exports = router;
