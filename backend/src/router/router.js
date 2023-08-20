14a994b57a54b33316ca79f4a9778e6403455ebf
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-20 19:41:16 -0300

    Modificación métodos en router

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getCities,
-  getCitiesId,
-  addCities,
-  deleteCities,
-  updateCities,
+  getCity,
+  getCityId,
+  addCity,
+  deleteCity,
+  updateCity,
 } = require("../controller/cityControllers");
 
 const { cityDataVerification } = require("../middlewares/cityVerification");
 //const { addAccount } = require("../controller/accountsController");
 
-router.get("/cities", getCities);
-router.get("/cities/:id", getCitiesId);
-router.post("/cities", cityDataVerification, addCities);
-router.delete("/cities", cityDataVerification, deleteCities);
-router.put("/cities", cityDataVerification, updateCities);
+router.get("/city", getCity);
+router.get("/city/:id", getCityId);
+router.post("/city", cityDataVerification, addCity);
+router.delete("/city", cityDataVerification, deleteCity);
+router.put("/city", cityDataVerification, updateCity);
 // router.post("/accounts", addAccount);
 
 module.exports = router;
