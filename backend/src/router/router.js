fadb2e4147f384d57667a438cb0cbc6a013150f1
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-22 16:45:37 -0300

    Acomodando proyecto (backend)

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -9,13 +9,11 @@ const {
 } = require("../controller/cityControllers");
 
 const { cityDataVerification } = require("../middlewares/cityVerification");
-//const { addAccount } = require("../controller/accountsController");
 
 router.get("/city", getCity);
 router.get("/city/:id", getCityId);
 router.post("/city", cityDataVerification, postCity);
 router.delete("/city", cityDataVerification, deleteCity);
 router.put("/city", cityDataVerification, updateCity);
-// router.post("/accounts", addAccount);
 
 module.exports = router;
