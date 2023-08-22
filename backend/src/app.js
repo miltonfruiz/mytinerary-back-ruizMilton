39cf94a8ba446f9cec0572a8a6fea4d0f4e0cbae
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-22 16:02:23 -0300

    Agregado de cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = require("./router/router");
+const cors = require("cors");
 require("./config/database");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
