7c0fe61913bf9a8dc8a617d72ff5cbaf3985da91
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-25 14:45:00 -0300

    Agregado de métodos en router

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -7,13 +7,31 @@ const {
   deleteCity,
   updateCity,
 } = require("../controller/cityControllers");
-
 const { cityDataVerification } = require("../middlewares/cityVerification");
+
+const {
+  getItinerary,
+  getItineraryName,
+  getItineraryId,
+  postItinerary,
+  updateItinerary,
+  deleteItinerary,
+} = require("../controller/itineraryControllers");
+const {
+  itineraryDataVerification,
+} = require("../middlewares/itineraryVerification");
 
 router.get("/city", getCity);
 router.get("/city/:id", getCityId);
 router.post("/city", cityDataVerification, postCity);
 router.delete("/city", cityDataVerification, deleteCity);
 router.put("/city", cityDataVerification, updateCity);
+
+router.get("/itinerary", getItinerary);
+router.get("/itinerary/:name", getItineraryName);
+router.get("/itinerary/:id", getItineraryId);
+router.post("/itinerary", itineraryDataVerification, postItinerary);
+router.put("/itinerary", itineraryDataVerification, updateItinerary);
+router.delete("/itinerary", itineraryDataVerification, deleteItinerary);
 
 module.exports = router;
