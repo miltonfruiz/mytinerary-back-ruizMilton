57987b3c6a0e49dd055f1d4914cb842c5c8a9c76
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-31 16:53:09 -0300

    Creación addCase(filter_city) en reducers.

--- frontend/src/store/reducers/city.js
+++ frontend/src/store/reducers/city.js
@@ -17,9 +17,14 @@ const initialState = {
 };
 
 const cityReducers = createReducer(initialState, (builder) => {
-  return builder.addCase(cityActions.get_city.fulfilled, (state, action) => {
-    const newState = { ...state, city: action.payload.city };
-    return newState;
-  });
+  return builder
+    .addCase(cityActions.get_city.fulfilled, (state, action) => {
+      const newState = { ...state, city: action.payload.city };
+      return newState;
+    })
+    .addCase(cityActions.filter_city.fulfilled, (state, action) => {
+      const newState = { ...state, city: [action.payload.city] };
+      return newState;
+    });
 });
 export default cityReducers;
