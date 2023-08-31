bf95de2f53f5897abdbc654435fc0758bc75a894
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-31 16:31:59 -0300

    get_city terminado y funcionando

--- frontend/src/store/actions/city.js
+++ frontend/src/store/actions/city.js
@@ -2,14 +2,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const get_city = createAsyncThunk("get_city", async () => {
-  const cities = await axios
-    .get("http://localhost:3000/api/city")
-    .then((response) => {
-      return response.data;
-    });
-  return {
-    city: cities,
-  };
+  try {
+    const cities = await axios
+      .get("http://localhost:3000/api/city")
+      .then((response) => {
+        return response.data;
+      });
+    return {
+      city: cities,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const cityActions = { get_city };
