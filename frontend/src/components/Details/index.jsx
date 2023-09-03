7327b2643ef25cae15db2db8381cd0f6b5958761
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-03 01:01:28 -0300

    Agregado de estilos a campos

--- frontend/src/components/Details/index.jsx
+++ frontend/src/components/Details/index.jsx
@@ -18,21 +18,21 @@ export default function Details({
           <div className="col-8">
             <img src={cardImage} className="imgDetails" alt="logo-img" />
           </div>
-          <div className="col-3">
-            <div className="">
-              <h6 className="h6">City</h6>
+          <div className="col-4">
+            <div className="campsContainer">
+              <h6 className="h6">City:</h6>
               <p className="p">{cardCity}</p>
-              <h6 className="h6">Country</h6>
+              <h6 className="h6">Country:</h6>
               <p className="p">{cardCountry}</p>
-              <h6 className="h6">Region</h6>
+              <h6 className="h6">Region:</h6>
               <p className="p">{cardRegion}</p>
-              <h6 className="h6">Population</h6>
+              <h6 className="h6">Population:</h6>
               <p className="p">{cardPopulation}</p>
-              <h6 className="h6">Badge</h6>
+              <h6 className="h6">Badge:</h6>
               <p className="h6">{cardBadge}</p>
-              <h6 className="h6">Monument</h6>
+              <h6 className="h6 monumentClass">Monument:</h6>
               <p className="p">{cardMonument}</p>
-              <h6 className="h6">Description</h6>
+              <h6 className="h6">Description:</h6>
               <p className="p">{cardDescription}</p>
             </div>
           </div>
