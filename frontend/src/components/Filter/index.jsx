7e892ea6483a500d7e91e0529fdb216c139432b2
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-03 03:29:00 -0300

    Creación de componente accordion

--- frontend/src/components/Filter/index.jsx
+++ frontend/src/components/Filter/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Details from "../Details";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import Accordion from "../Accordion";
 
 export default function Filter() {
   let cityInStores = useSelector((store) => store.cityReducer.city);
@@ -11,7 +12,7 @@ export default function Filter() {
         <div className="row justify-content-center d-flex ">
           <h2 className="col-8 mb-5">Details</h2>
         </div>
-        <div className="row justify-content-center d-flex ">
+        <div className="row justify-content-center d-flex">
           {cityInStores.map((array) => (
             <Details
               cardImage={cityInStores[0].itineraries[0].images}
@@ -25,6 +26,9 @@ export default function Filter() {
               cardName={cityInStores[0].itineraries[0].name}
             />
           ))}
+        </div>
+        <div className="row justify-content-center d-flex">
+          <Accordion />
         </div>
       </section>
     </>
