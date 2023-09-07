07db2c580f189545d3af797a6e16c90d43c3e53f
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-07 20:47:19 -0300

    Validación de datos

--- backend/src/middlewares/usersVerification.js
+++ backend/src/middlewares/usersVerification.js
@@ -1,1 +1,12 @@
+const Joi = require("joi");
+
+const userSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  lastname: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(10).max(20).email().required(),
+  password: Joi.string().min(6).max(16).alphanum().required(),
+  images: Joi.string().uri().required(),
+  country: Joi.string().min(5).max(20).required(),
+});
+
 const userDataVerification = (req, res, next) => {};
