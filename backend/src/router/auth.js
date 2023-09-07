85cc209cc6c35898e0eac3f8cd13cc187e04e287
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-07 16:20:06 -0300

    Creación auth.js (router)

--- backend/src/router/auth.js
+++ backend/src/router/auth.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { userCreator } = require("../controller/usersControllers");
 const authRouter = express.Router();
+
+authRouter.post("/userCreator", userCreator);
 
 module.exports = authRouter;
