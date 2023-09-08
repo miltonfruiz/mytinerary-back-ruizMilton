bab8a45d12d298b6f6eb540df6940d6f295a1d44
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-08 14:56:07 -0300

    Validadores de userSchema terminado

--- backend/src/middlewares/usersVerification.js
+++ backend/src/middlewares/usersVerification.js
@@ -7,11 +7,11 @@ const userSchema = Joi.object({
     "string.empty": "x ¡Empty name, please try again! x",
     "any.required": "x ¡Name required! x",
   }),
-  lastname: Joi.string().min(3).max(20).required().messages({
+  lastName: Joi.string().min(4).max(21).required().messages({
     "string.min": "x ¡The lastname must be at least 3 characters long! x",
     "string.max": "x ¡The lastname must have a maximum of 20 characters! x",
     "string.empty": "x ¡Empty lastname, please try again! x",
-    "any.required": "x Lastname required! x",
+    "any.required": "x ¡Lastname required! x",
   }),
   email: Joi.string().min(11).max(30).email().required().messages({
     "string.min": "x ¡The email must be at least 11 characters long! x",
@@ -29,9 +29,9 @@ const userSchema = Joi.object({
     "any.required": "x ¡Password required! x",
   }),
   images: Joi.string().uri().required().messages({
-    "string.uri": "x ¡Please enter your uri! x",
-    "string.empty": "x ¡Empty uri, please try again! x",
-    "any.required": "x ¡Uri required! x",
+    "string.uri": "x ¡Please load your URL! x",
+    "string.empty": "x ¡Empty images, please try again! x",
+    "any.required": "x ¡URL required! x",
   }),
   country: Joi.string().min(5).max(25).required().messages({
     "string.min": "x ¡The country must be at least 4 characters long! x",
@@ -43,7 +43,7 @@ const userSchema = Joi.object({
 
 const userDataVerification = (req, res, next) => {
   const payload = req.body;
-  const validatedUser = userSchema.validate(payload);
+  const validatedUser = userSchema.validate(payload, { abortEarly: false });
   if (validatedUser.error) {
     return res.status(400).json({
       message: validatedUser.error.details.map((err) => err.message),
