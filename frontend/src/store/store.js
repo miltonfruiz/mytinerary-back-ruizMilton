1d01e3bc317f138b457ca47f5b50146bc8164a38
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-13 18:39:56 -0300

    Actualización de store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cityReducers from "./reducers/city";
+import userReducer from "./reducers/user";
 
 export const store = configureStore({
   reducer: {
     cityReducer: cityReducers,
+    userReducer: userReducer,
   },
 });
