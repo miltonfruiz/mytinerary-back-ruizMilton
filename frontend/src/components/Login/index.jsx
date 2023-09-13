328f947b1f177fe5c5baf69a1799aa8d72be10c0
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-13 16:25:26 -0300

    Agregando estilos a login

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,51 +1,75 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./style.css";
+import axios from "axios";
 
 export default function LogIn() {
+  let inputEmail = useRef();
+  let inputPassword = useRef();
+
+  let handleSignIn = () => {
+    axios
+      .post("http://localhost:3000/api/user/login", {
+        email: inputEmail.current.value,
+        password: inputPassword.current.value,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) =>
+        error.response.data.message.forEach((message) => console.log(message))
+      );
+  };
   return (
     <>
       <div className="row justify-content-center d-flex ">
         <h2 className="col-8 mt-5 mb-5">Login</h2>
       </div>
-      <form className="col align-self-center">
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+      <div className="row justify-content-center d-flex divFormContainer">
+        <form className="col-3 align-self-center formContainer">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label mt-5">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              ref={inputEmail}
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
           </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              ref={inputPassword}
+            />
+          </div>
+          <div className="mb-3 ms-2 mt-4">
+            <a
+              href="/register"
+              className="form-check-label createClass"
+              htmlFor="exampleCheck1"
+            >
+              * Create Account
+            </a>
+          </div>
+          <button
+            onClick={() => handleSignIn()}
+            type="submit"
+            className="btn btn-outline-primary buttonSignIn mb-4"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
     </>
   );
 }
