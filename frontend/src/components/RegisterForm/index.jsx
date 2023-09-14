04f1e991f9df6b1bfde7e67b1184c590e2d86662
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-14 05:19:30 -0300

    RegisterForm terminado

--- frontend/src/components/RegisterForm/index.jsx
+++ frontend/src/components/RegisterForm/index.jsx
@@ -13,20 +13,20 @@ export default function RegisterForm() {
         <h2 className="col-8 mt-5 mb-5">Register</h2>
       </div>
       <div className="row justify-content-center d-flex">
-        <form class="row g-3 formRegister">
-          <div class="col-12">
+        <form className="formRegister">
+          <div className="col-12 mt-3">
             <label for="inputEmail4" class="form-label">
               Email
             </label>
             <input type="email" class="form-control" id="inputEmail4" />
           </div>
-          <div class="col-12 mt-0">
+          <div className="col-12 mt-2">
             <label for="inputPassword4" class="form-label">
               Password
             </label>
             <input type="password" class="form-control" id="inputPassword4" />
           </div>
-          <div class="col-12 mt-0">
+          <div className="col-12 mt-2">
             <label for="inputAddress" class="form-label">
               Name
             </label>
@@ -37,7 +37,7 @@ export default function RegisterForm() {
               placeholder="1234 Main St"
             />
           </div>
-          <div class="col-12 mt-0">
+          <div className="col-12 mt-2">
             <label for="inputAddress2" class="form-label">
               Last Name
             </label>
@@ -48,24 +48,27 @@ export default function RegisterForm() {
               placeholder="Apartment, studio, or floor"
             />
           </div>
-          <div class="col-12 mt-0">
+          <div className="col-12 mt-2">
             <label for="inputCity" class="form-label">
               Image
             </label>
             <input type="text" class="form-control" id="inputCity" />
           </div>
-          <div class="col-12 mt-0">
-            <label for="inputState" class="form-label">
+          <div className="col-12 mt-2">
+            <label for="inputState" className="form-label">
               Country
             </label>
-            <select id="inputState" class="form-select">
+            <select id="inputState" className="form-select">
               <option selected>Choose...</option>
               <option>...</option>
             </select>
           </div>
-          <div class="col-12 mt-0">
-            <button type="submit" className="btn btn-primary mb-3">
-              Sign in
+          <div className="col-12 mt-2">
+            <button
+              type="submit"
+              className="btn btn-outline-primary mb-4 mt-3 buttonRegister"
+            >
+              Register
             </button>
             <GoogleLogin
               onSuccess={signUpGoogle}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
 import LogIn from "../components/LogIn";
 
 export default function Login() {
   return (
     <>
-      <Header />
       <LogIn />
       <Footer />
     </>
