0085596e8a2dfa2cb81dcfb20bd33a374f07060e
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-14 12:48:09 -0300

    Creación handleRegister()

--- frontend/src/components/RegisterForm/index.jsx
+++ frontend/src/components/RegisterForm/index.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import userActions from "../../store/actions/user";
 import "./style.css";
 import { GoogleLogin } from "@react-oauth/google";
 import jwtdecode from "jwt-decode";
 
 export default function RegisterForm() {
+  let inputName = useRef();
+  let inputLastName = useRef();
+  let inputEmail = useRef();
+  let inputPassword = useRef();
+  let inputImages = useRef();
+  let inputCountry = useRef();
+  const dispatch = useDispatch();
+
+  let handleRegister = () => {
+    dispatch(
+      userActions.register({
+        name: inputName.current.value,
+        lastName: inputLastName.current.value,
+        email: inputEmail.current.value,
+        password: inputPassword.current.value,
+        images: inputImages.current.value,
+        country: inputCountry.current.value,
+      })
+    );
+  };
+
   const signUpGoogle = (credentialResponse) => {
     let dataUser = jwtdecode(credentialResponse.credential);
   };
@@ -15,58 +38,76 @@ export default function RegisterForm() {
       <div className="row justify-content-center d-flex">
         <form className="formRegister">
           <div className="col-12 mt-3">
-            <label for="inputEmail4" class="form-label">
+            <label htmlFor="inputEmail4" className="form-label">
               Email
             </label>
-            <input type="email" class="form-control" id="inputEmail4" />
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail4"
+              ref={inputEmail}
+            />
           </div>
           <div className="col-12 mt-2">
-            <label for="inputPassword4" class="form-label">
+            <label htmlFor="inputPassword4" className="form-label">
               Password
             </label>
-            <input type="password" class="form-control" id="inputPassword4" />
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword4"
+              ref={inputPassword}
+            />
           </div>
           <div className="col-12 mt-2">
-            <label for="inputAddress" class="form-label">
+            <label htmlFor="inputAddress" className="form-label">
               Name
             </label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="inputAddress"
-              placeholder="1234 Main St"
+              ref={inputName}
             />
           </div>
           <div className="col-12 mt-2">
-            <label for="inputAddress2" class="form-label">
+            <label htmlFor="inputAddress2" className="form-label">
               Last Name
             </label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="inputAddress2"
-              placeholder="Apartment, studio, or floor"
+              ref={inputLastName}
             />
           </div>
           <div className="col-12 mt-2">
-            <label for="inputCity" class="form-label">
+            <label htmlFor="inputCity" className="form-label">
               Image
             </label>
-            <input type="text" class="form-control" id="inputCity" />
+            <input
+              type="text"
+              className="form-control"
+              id="inputCity"
+              ref={inputImages}
+            />
           </div>
           <div className="col-12 mt-2">
-            <label for="inputState" className="form-label">
+            <label htmlFor="inputState" className="form-label">
               Country
             </label>
             <select id="inputState" className="form-select">
-              <option selected>Choose...</option>
-              <option>...</option>
+              <option>Choose...</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
             </select>
           </div>
           <div className="col-12 mt-2">
             <button
               type="submit"
               className="btn btn-outline-primary mb-4 mt-3 buttonRegister"
+              onClick={() => handleRegister()}
             >
               Register
             </button>
