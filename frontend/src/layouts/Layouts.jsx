d33287fda890f7b087ce46843ee174e73e5a958e
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-16 16:43:45 -0300

    Agregado  sweetalert

--- frontend/src/layouts/Layouts.jsx
+++ frontend/src/layouts/Layouts.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function Layouts() {
-  let token = localStorage.getItem("token");
-  console.log(token);
-  if (!token) {
-    return <Navigate to={"/login"} />;
-  }
   return (
     <>
       <Outlet />
--- frontend/src/pages/Cities.jsx
+++ frontend/src/pages/Cities.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Fetch from "../components/Fetch";
+import { Navigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function Cities() {
+  let token = localStorage.getItem("token");
+  if (!token) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "¡Need login to access!",
+    });
+    return <Navigate to={"/login"} />;
+  }
   return (
     <>
       <Header />
