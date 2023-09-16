949e9f9cab04592486a76c4a95f9de882c539f4a
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-16 02:23:41 -0300

    Acomodando login y register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Footer from "../components/Footer";
 import LogIn from "../components/LogIn";
 
 export default function Login() {
   return (
     <>
       <LogIn />
-      <Footer />
     </>
   );
 }
