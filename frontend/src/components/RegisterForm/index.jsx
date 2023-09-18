efd8e1fd8a4fca44d79cae664b9295eb859e6e1c
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-09-18 02:05:58 -0300

    Agregado imagen de fonde en registro

--- frontend/src/components/RegisterForm/index.jsx
+++ frontend/src/components/RegisterForm/index.jsx
@@ -48,94 +48,96 @@ export default function RegisterForm() {
   };
   return (
     <>
-      <div className="row justify-content-center d-flex ">
-        <h2 className="col-8 mt-5 mb-5">Register</h2>
-      </div>
-      <div className="row justify-content-center d-flex">
-        <form className="formRegister" onSubmit={handlerRegister}>
-          <div className="col-12 mt-3">
-            <label htmlFor="inputEmail4" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="inputEmail4"
-              ref={inputEmail}
-            />
-          </div>
-          <div className="col-12 mt-2">
-            <label htmlFor="inputPassword4" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="inputPassword4"
-              ref={inputPassword}
-            />
-          </div>
-          <div className="col-12 mt-2">
-            <label htmlFor="inputAddress" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputAddress"
-              ref={inputName}
-            />
-          </div>
-          <div className="col-12 mt-2">
-            <label htmlFor="inputAddress2" className="form-label">
-              Last Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputAddress2"
-              ref={inputLastName}
-            />
-          </div>
-          <div className="col-12 mt-2">
-            <label htmlFor="inputCity" className="form-label">
-              Image
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputCity"
-              ref={inputImages}
-            />
-          </div>
-          <div className="col-12 mt-2">
-            <label htmlFor="inputState" className="form-label">
-              Country
-            </label>
-            <select id="inputState" className="form-select">
-              <option>Choose country...</option>
-              {cityInStore.map((country) => (
-                <option ref={inputCountry}>{country.country}</option>
-              ))}
-            </select>
-          </div>
-          <div className="col-12 mt-2">
-            <button
-              type="submit"
-              className="btn btn-outline-primary mb-4 mt-3 buttonRegister"
-            >
-              Register
-            </button>
-            <GoogleLogin
-              text="signup_with"
-              size="large "
-              onSuccess={signUpGoogle}
-              onError={() => {
-                console.log("Login Failed");
-              }}
-            />
-          </div>
-        </form>
+      <div className="registerContainer">
+        <div className="row justify-content-center d-flex ">
+          <h2 className="col-8 mt-5 mb-5 text-white registerText">Register</h2>
+        </div>
+        <div className="row justify-content-center d-flex">
+          <form className="formRegister mb-5" onSubmit={handlerRegister}>
+            <div className="col-12 mt-3">
+              <label htmlFor="inputEmail4" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail4"
+                ref={inputEmail}
+              />
+            </div>
+            <div className="col-12 mt-2">
+              <label htmlFor="inputPassword4" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword4"
+                ref={inputPassword}
+              />
+            </div>
+            <div className="col-12 mt-2">
+              <label htmlFor="inputAddress" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress"
+                ref={inputName}
+              />
+            </div>
+            <div className="col-12 mt-2">
+              <label htmlFor="inputAddress2" className="form-label">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress2"
+                ref={inputLastName}
+              />
+            </div>
+            <div className="col-12 mt-2">
+              <label htmlFor="inputCity" className="form-label">
+                Image
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputCity"
+                ref={inputImages}
+              />
+            </div>
+            <div className="col-12 mt-2">
+              <label htmlFor="inputState" className="form-label">
+                Country
+              </label>
+              <select id="inputState" className="form-select">
+                <option>Choose country...</option>
+                {cityInStore.map((country) => (
+                  <option ref={inputCountry}>{country.country}</option>
+                ))}
+              </select>
+            </div>
+            <div className="col-12 mt-2">
+              <button
+                type="submit"
+                className="btn btn-outline-primary mb-4 mt-3 buttonRegister"
+              >
+                Register
+              </button>
+              <GoogleLogin
+                text="signup_with"
+                size="large "
+                onSuccess={signUpGoogle}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
